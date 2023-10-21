ae8c560d1bae95e40cd7ffa4639dd522604dcf84
Windy Deng <windy0deng@gmail.com>
2023-10-21 23:13:50 +0800

    Update `README.md`.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# ClasswizDisplayFont
-Font for screen display of CASIO ClassWiz calculator series.
+# CASIO Classwiz 屏幕显示字体
+
+**来自卡西欧计算器屏幕上的像素字体！**
+
+![ClassWizDisplay](readme_assets/ClassWizDisplay.svg)
+
+## 食用方法
+
+字体分为四部分：X Display、CW Display、Math X 与 Math CW。带有「Display」的字体用作普通文本，提供 OTF 与 TTF 格式；带有「Math」的字体由 Opentype MATH 表强力驱动，用作数学公式输入，仅提供 OTF 格式。
+
+打包好的字体可以到 [Release 页面](https://github.com/Wenti-D/ClasswizDisplayFont/releases)获取。本文只是关于字体的一个粗略说明，而**压缩包内除字体外还有自述文件（`README.pdf` 之类的文档），使用前请一定、务必、绝对要认真、仔细、用心阅读！**
+
+## 构建字体
+
+### Display 部分
+
+Display 部分的字体源文件以[统一字体对象 3（UFO 3）](https://unifiedfontobject.org/versions/ufo3/index.html)格式提供，看起来像一个以 `.ufo` 结尾的文件夹。很多字体设计软件都可以读取 UFO 字体，例如 [Fontforge](https://fontforge.org/)，欲编辑字体，可以下载它。但若只是想构建字体，则 Google 的 [`fontmake`](https://github.com/googlefonts/fontmake) 工具集也可以处理，它依赖 Python 工作，所以记得预先安装 Python 与 `pip`。
+
+此存储库为 Windows 系统用户制作了一个构建脚本，Windows 用户可以做至以下第 1 步，然后直接双击 `build.bat`即可。接下来是利用 `fontmake` 构建字体的详细流程：
+
+0. 安装 Python，以及 `pip`。
+1. 将本项目克隆到一个合适的位置。
+   
+        git clone https://github.com/Wenti-D/ClasswizDisplayFont
+
+2. 建议创建一个 Python 虚拟环境：
+   
+        python -m venv <your_venv_name>
+    
+    Windows 平台使用以下命令激活：
+
+        ./<your_venv_name>/Script/activate
+        
+    Linux 平台使用以下命令激活：
+    
+        source <your_venv_name>/bin/activate
+
+3. 安装依赖：
+
+        python -m pip install -r requirements.txt
+
+4. 构建字体：
+        
+        fontmake -u ClassWizXDisplay-Regular.ufo --output-dir output
+        fontmake -u ClassWizCWDisplay-Regular.ufo --output-dir output
+
+    构建好的字体将会位于 `output` 文件夹内。
+
+### Math 部分
+
+Math 部分的字体源文件以 [Fontforge](https://fontforge.org/) 源文件格式提供，是一个扩展名为 `.sfd` 的文件。欲编辑、构建字体，请下载 Fontforge。
+
+## 贡献与反馈
+
+如有任何问题或建议，还请开启一个议题（issue）。
+
---
+++ build.bat
@@ -0,0 +1,77 @@
+@echo off
+cd /d %~dp0
+
+set VENV_DIR=%~dp0fontmake_venv
+set PYTHON=python
+
+%PYTHON% -c "" >output.log 2>error.log
+if %ERRORLEVEL% == 0 goto :check_pip
+echo Couldn't launch python.
+goto :show_output_error
+
+:check_pip
+%PYTHON% -m pip --help >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :create_venv
+echo Couldn't find pip.
+goto :show_output_error
+
+:create_venv
+dir "%VENV_DIR%\Scripts\Python.exe" >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :activate_venv
+
+for /f "delims=" %%i in ('CALL python -c "import sys; print(sys.executable)"') do set PYTHON_FULLPATH="%%i"
+echo Creating venv in %VENV_DIR% using python at %PYTHON_FULLPATH%.
+%PYTHON% -m venv "%VENV_DIR%" >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :activate_venv
+echo Cannot create venv in "%VENV_DIR%".
+goto :show_output_error
+
+:activate_venv
+set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+echo venv %VENV_DIR%.
+
+:install_fontmake
+echo Installing fontmake.
+%PYTHON% -m pip install --upgrade pip >>output.log 2>>error.log
+%PYTHON% -m pip install -r requirements.txt >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :build_font_x
+echo An error occured in installing requirements.
+goto :show_output_error
+
+:build_font_x
+set OUTPUT_DIR=output
+set FONT_NAME=ClassWizXDisplay-Regular
+echo Building font X Display into "%OUTPUT_DIR%" folder.
+fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :build_font_cw
+
+:build_font_cw
+set FONT_NAME=ClassWizCWDisplay-Regular
+echo Building font CW Display into "%OUTPUT_DIR%" folder.
+fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 goto :end_success
+
+:show_output_error
+echo.
+echo Exit code: %ERRORLEVEL%.
+
+for /f %%f in (output.log) do set size=%%~zf
+if %size% equ 0 goto :show_error
+echo.
+echo output.log:
+type output.log
+
+:show_error
+for /f %%f in (error.log) do set size=%%~zf
+if %size% equ 0 goto :end
+echo.
+echo error.log:
+type error.log
+
+:end_success
+echo.
+echo Done.
+
+:end
+pause
+exit /b %ERRORLEVEL%
