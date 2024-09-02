d31e589837348e4c1ef33cf3171ff8830bb00bb2
Windy Deng <windy0deng@gmail.com>
2024-09-02 17:56:11 +0800

    Refine build script.

--- build.bat
+++ build.bat
@@ -30,45 +30,81 @@ goto :show_output_error
 set PYTHON="%VENV_DIR%\Scripts\Python.exe"
 echo venv %VENV_DIR%.
 
+:detect_fontmake
+fontmake -h >>output.log 2>>error.log
+if %ERRORLEVEL% == 0 (
+    echo Detected fontmake.
+    goto :clean_output_folder
+) else (
+    echo No fontmake installed.
+)
+
 :install_fontmake
-echo Installing fontmake.
+echo Configuring fontmake...
 %PYTHON% -m pip install --upgrade pip >>output.log 2>>error.log
 %PYTHON% -m pip install -r requirements.txt >>output.log 2>>error.log
-if %ERRORLEVEL% == 0 goto :build_font_x
-echo An error occured in installing requirements.
+if %ERRORLEVEL% == 0 goto :clean_output_folder
+echo An error occured when installing requirements.
 goto :show_output_error
 
-:build_font_x
+:clean_output_folder
 set OUTPUT_DIR=output
+rd /s /q %OUTPUT_DIR% >>output.log 2>>error.log
+if %ERRORLEVEL% neq 0 (
+    if %ERRORLEVEL% neq 2 (
+        goto :show_output_error
+    )
+)
+
+:build_font_x
 set FONT_NAME=ClassWizXDisplay-Regular
-echo Building font X Display into "%OUTPUT_DIR%" folder.
+echo Building font X Display into "%OUTPUT_DIR%" folder...
 fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
-%PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >output.log 2>error.log
+if %ERRORLEVEL% neq 0 goto :show_output_error
+%PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >>output.log 2>>error.log
 if %ERRORLEVEL% == 0 goto :build_font_cw
 
 :build_font_cw
 set FONT_NAME=ClassWizCWDisplay-Regular
-echo Building font CW Display into "%OUTPUT_DIR%" folder.
+echo Building font CW Display into "%OUTPUT_DIR%" folder...
 fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
-%PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >output.log 2>error.log
+if %ERRORLEVEL% neq 0 goto :show_output_error
+%PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >>output.log 2>>error.log
 if %ERRORLEVEL% == 0 goto :end_success
 
 :show_output_error
-echo.
-echo Exit code: %ERRORLEVEL%.
-
-for /f %%f in (output.log) do set size=%%~zf
+set prev_err_lvl=%ERRORLEVEL%
+for /f %%f in ("output.log") do set size=%%~zf
 if %size% equ 0 goto :show_error
 echo.
-echo output.log:
+echo ########################################
+echo # output.log:
+echo ########################################
+echo.
 type output.log
+echo.
 
 :show_error
-for /f %%f in (error.log) do set size=%%~zf
-if %size% equ 0 goto :end
+for /f %%f in ("error.log") do set size=%%~zf
+if %size% equ 0 goto :end_failed
 echo.
-echo error.log:
+echo ########################################
+echo # error.log:
+echo ########################################
+echo.
 type error.log
+echo.
+
+echo !!!
+echo Failed due to an error, please refer to error.log above or file.
+echo Exit code: %prev_err_lvl%.
+goto :end
+
+:end_failed_no_error
+echo !!!
+echo Failed due to an error.
+echo Exit code: %prev_err_lvl%.
+goto :end
 
 :end_success
 echo.
