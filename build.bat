e2aa0e29a3764c0ee252f7da122af25b8c864f05
Windy Deng <windy0deng@gmail.com>
2025-06-30 15:48:22 +0800

    Update X/CW Display v3.006/v3.007, fix most metric issues.

--- build.bat
+++ build.bat
@@ -28,10 +28,11 @@ goto :show_output_error
 
 :activate_venv
 set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+set FONTMAKE="%VENV_DIR%\Scripts\fontmake.exe"
 echo venv %VENV_DIR%.
 
 :detect_fontmake
-fontmake -h >>output.log 2>>error.log
+%FONTMAKE% -h >>output.log 2>>error.log
 if %ERRORLEVEL% == 0 (
     echo Detected fontmake.
     goto :clean_output_folder
@@ -59,7 +60,7 @@ if %ERRORLEVEL% neq 0 (
 :build_font_x
 set FONT_NAME=ClassWizXDisplay-Regular
 echo Building font X Display into "%OUTPUT_DIR%" folder...
-fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
+%FONTMAKE% -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
 if %ERRORLEVEL% neq 0 goto :show_output_error
 %PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >>output.log 2>>error.log
 if %ERRORLEVEL% == 0 goto :build_font_cw
@@ -67,7 +68,7 @@ if %ERRORLEVEL% == 0 goto :build_font_cw
 :build_font_cw
 set FONT_NAME=ClassWizCWDisplay-Regular
 echo Building font CW Display into "%OUTPUT_DIR%" folder...
-fontmake -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
+%FONTMAKE% -u %FONT_NAME%.ufo --output-dir %OUTPUT_DIR% >>output.log 2>>error.log
 if %ERRORLEVEL% neq 0 goto :show_output_error
 %PYTHON% -c "from fontTools.ttLib.woff2 import compress; compress('%OUTPUT_DIR%/%FONT_NAME%.otf', '%OUTPUT_DIR%/%FONT_NAME%.woff2')" >>output.log 2>>error.log
 if %ERRORLEVEL% == 0 goto :end_success
